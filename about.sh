f41021b48e8474c6edf55dc72c382390ed8781cd
XseCurity-HTW <70092703+XseCurity-HTW@users.noreply.github.com>
2020-09-04 14:44:33 +0530

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,34 @@
+lsclear
+echo -e '\e[96m                       ----------------'
+echo -e '\e[92m                       ┌─┐┌┐ ┌─┐┬ ┬┌┬┐
+                       ├─┤├┴┐│ ││ │ │ 
+                       ┴ ┴└─┘└─┘└─┘ ┴ '
+echo -e '\e[96m                       ----------------'
+echo -e '\e[96m      |------------------|'
+echo -e '\e[96m      |'
+echo -e '\e[96m      |'
+sleep 1.5
+echo -e '\e[96m     [\e[92m+\e[96m]-------[\e[92mABHI\e[96m]'
+echo -e '\e[96m      |'
+echo -e '\e[96m      |'
+sleep 1.0
+echo -e '\e[96m     [\e[92m+\e[96m]-------[\e[92mPOWERFUL\e[96m]'
+echo -e '\e[96m      |'
+echo -e '\e[96m      |'
+echo -e '\e[96m      |'
+sleep 2.0
+echo -e '\e[96m     [\e[92m+\e[96m]--------------'
+echo -e '\e[96m                      |'
+echo -e '\e[92m  THIS IS ONLY FOR EDUCATIONAL PURPOSE SO'
+echo -e '\e[92m IM NOT RESPONSIBLE IF YOU DO ANY ILLEGAL THINGS'
+echo -e '\e[92m   THANKS FOR READING PLEASE SUBSCRIBE {White hat Hacker56}'
+echo -e '\e[92m           HAVE A GOOD DAY GUYS :)'
+echo -e '\e[96m                      |'
+echo -e '\e[96m                      |'
+sleep 4.5
+echo -e '\e[96m                     [\e[92m+\e[96m]------------[\e[92mBYE\e[96m]\e[0m'
+sleep 2.0
+cd $HOME
+clear
+bash mr.phish
+
